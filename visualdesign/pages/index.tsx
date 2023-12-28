aa9c901c9f18137f7580e979aee710618dc07bb2
Igor-Moens <igor.moens@student.ucll.be>
2023-12-28 14:17:48 +0100

    flowbite + components + pages added

---
+++ visualdesign/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+                <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+            </Head>
+            <main>
+                <p>HOME</p>
+            </main>
+        </>
+    );
+};
+
+export default Home;
