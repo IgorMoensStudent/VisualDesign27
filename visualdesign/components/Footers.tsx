c4b3cf8e4de4af411ac8c7e5f6f48db00d78a473
EricSap <91263854+EricSap@users.noreply.github.com>
2023-12-29 17:24:02 +0100

    final changes home page

--- visualdesign/components/Footers.tsx
+++ visualdesign/components/Footers.tsx
@@ -1,48 +1,29 @@
 import React from "react";
 import { Footer } from 'flowbite-react';
 import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+import Logo from '../public/UCLL.png';
 
 const Footers : React.FC = () => {
     return (
         <Footer container>
-            <div className="w-full">
-                <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-                <div>
-                    <Footer.Brand
-                    href="https://flowbite.com"
-                    src="https://flowbite.com/docs/images/logo.svg"
-                    alt="Flowbite Logo"
-                    name="Flowbite"
-                    />
-                </div>
-                <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                    <div>
-                    <Footer.Title title="about" />
-                    <Footer.LinkGroup col>
-                        <Footer.Link href="#">Business</Footer.Link>
-                        <Footer.Link href="#">Methology</Footer.Link>
-                        <Footer.Link href="#">Partners</Footer.Link>
-                    </Footer.LinkGroup>
-                    </div>
-                    <div>
-                    <Footer.Title title="Follow us" />
-                    <Footer.LinkGroup col>
-                        <Footer.Link href="#">Blog</Footer.Link>
-                        <Footer.Link href="#">Newspaper</Footer.Link>
-                    </Footer.LinkGroup>
-                    </div>
-                    <div>
-                    <Footer.Title title="Legal" />
-                    <Footer.LinkGroup col>
-                        <Footer.Link href="#">Privacy Policy</Footer.Link>
-                        <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-                    </Footer.LinkGroup>
-                    </div>
-                </div>
+            <div className="w-full text-center">
+                <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+                <Footer.Brand
+                    href="https://www.ucll.be/nl"
+                    src={Logo.src}
+                    alt="UCLL Logo"
+                    
+                />
+                <Footer.LinkGroup>
+                    <Footer.Link href="#">About</Footer.Link>
+                    <Footer.Link href="#">Privacy Policy</Footer.Link>
+                    <Footer.Link href="#">Licensing</Footer.Link>
+                    <Footer.Link href="#">Contact</Footer.Link>
+                </Footer.LinkGroup>
                 </div>
                 <Footer.Divider />
                 <div className="w-full sm:flex sm:items-center sm:justify-between">
-                <Footer.Copyright href="#" by="Precision Cycling™" year={2024} />
+                <Footer.Copyright href="#" by="Igor Moens and Eric Sarpong™" year={2024} />
                 <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
                     <Footer.Icon href="#" icon={BsFacebook} />
                     <Footer.Icon href="#" icon={BsInstagram} />
